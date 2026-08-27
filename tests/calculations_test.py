67d752169614c80b0cdbc7752478945de720080d
spvimal <spvimalmca@hotmail.com>
2026-08-27 12:06:23 +0000

    new test

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -68,3 +68,13 @@ def test_get_nth_fibonacci_one():
 
 #     # Assert
 #     assert result == 89
+def test_get_nth_fibonacci_ten():
+    """Test with n=10."""
+    # Arrange
+    n = 10
+
+    # Act
+    result = get_nth_fibonacci(n)
+
+    # Assert
+    assert result == 55
